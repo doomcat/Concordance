c2db8872c135efb8a8a3f02af96f54acad869932
Owain Jones <tehdoomcat@gmail.com>
2011-10-23 22:43:08 +0100

    better debugging

--- src/uk/ac/aber/dcs/odj/Run.java
+++ src/uk/ac/aber/dcs/odj/Run.java
@@ -5,16 +5,24 @@ import java.util.Enumeration;
 
 public class Run {
 	static final String[] index = {
-		"Jesus"
+		"and"
 	};
 	
 	public static void main(String[] args) {
-		final long startTime = System.nanoTime();
-		final long endTime;
+		final long instTime, scanTime, iterTime;
+		final long endInst, endScan, endIter;
+		final long startFreeMem = Runtime.getRuntime().freeMemory(), endFreeMem;
 		try {
+			instTime = System.nanoTime();
 			Concordance concordance = new Concordance("./kingjames_bible.txt",
 					index);
+			endInst = System.nanoTime()-instTime;
+			
+			scanTime = System.nanoTime();
 			concordance.scan();
+			endScan = System.nanoTime()-scanTime;
+			
+			iterTime = System.nanoTime();
 			for(String key : concordance.keySet()) {
 				System.out.println(key.toUpperCase());
 				LittleLinkedList list = concordance.get(key);
@@ -28,13 +36,15 @@ public class Run {
 				}
 				System.out.println();
 			}
+			endIter = System.nanoTime()-iterTime;
+			
+			endFreeMem = Runtime.getRuntime().freeMemory();
+			System.out.println("INST: "+endInst+", SCAN: "+endScan+", ITER: "+endIter);
+			System.out.println("SMEM: "+startFreeMem+", EMEM: "+endFreeMem);
 		} catch (IOException e) {
 			System.out.println("File loading error:");
 			e.printStackTrace();
-		} finally {
-			endTime = System.nanoTime();
 		}
-		System.out.println("Time taken: "+(endTime-startTime));
 	}
 
 }
